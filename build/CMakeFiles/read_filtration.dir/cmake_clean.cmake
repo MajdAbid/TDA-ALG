1aeaaf1e9a25d6ec2f0490107298d8d265ee5560
abid_majd <majd.abidd@gmail.com>
2023-10-19 00:56:14 +0200

    first implementation of cmake

---
+++ build/CMakeFiles/read_filtration.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_filtration.dir/src/read_filtration.cpp.o"
+  "CMakeFiles/read_filtration.dir/src/read_filtration.cpp.o.d"
+  "bin/read_filtration"
+  "bin/read_filtration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_filtration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
